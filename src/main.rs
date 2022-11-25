84549735c17f20a329e4596e3b9d4f9b73ac17c8
Andrija Mitrovic <andrija1923@gmail.com>
2022-11-25 23:40:03 +0100

    Returning a Result Instead of Calling panic!

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args);
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    let query = config.as_ref().unwrap().query.clone();
+    let file_path = config.as_ref().unwrap().file_path.clone();
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 
     let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
@@ -23,16 +26,16 @@ struct Config
 
 impl Config 
 {
-    fn new(args: &[String]) -> Config 
+    fn build(args: &[String]) -> Result<Config, &'static str>
     {
         if args.len() < 3 
         {
-            panic!("Not enough arguments!");
+            return Err("Not enough arguments!");
         };
 
-        Config {
+        Ok(Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
-        }
+        })
     }
 }
