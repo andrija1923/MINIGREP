0eef02ff3ce26131030a6a2890fdacbe2fced884
Andrija Mitrovic <andrija1923@gmail.com>
2022-11-26 21:40:43 +0100

    Returning Errors from the run Function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,12 +18,14 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config)
+fn run(config: Config) -> Result<(), Box<dyn Error>>
 {
     let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+        fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config 
